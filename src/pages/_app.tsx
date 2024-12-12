6d67697015ac40a4bba25373c19bd9387d93cad6
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-12-12 22:57:55 +0300

    TSK-44: добавить сброс css стилей

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import { theme } from "@/styles";
-import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
+import theme from '@/styles'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />;
     </ChakraProvider>
-  );
+  )
 }
