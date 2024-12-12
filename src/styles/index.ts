5d346a8ac7bd92a242b44f2774d324de2500beec
anastasia.peretyatko <aperetyatko989@gmail.com>
2024-12-12 22:55:36 +0300

    TSK-44: инициализировать конфиг темы

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,28 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
+import { containerTheme } from './components/container';
+import { buttonTheme } from './components/button';
 
-export const theme = extendTheme({});
+const theme = extendTheme({
+  colors: {
+    PRIMARY_BLUE: '#2A85FF',
+    PRIMARY_GREEN: '#83BF6E',
+    PRIMARY_ORANGE: '#FF6A55',
+    PRIMARY_PURPLE: '#8E59FF',
+
+    SECONDARY_BLUE: '#B1E5FC',
+    SECONDARY_GREEN: '#B5E4CA',
+    SECONDARY_ORANGE: '#FFBC99',
+    SECONDARY_PURPLE: '#CABDFF',
+
+    BLACK_100: '#111315',
+    BLACK_200: '#1A1D1F',
+    BLACK_300: '#272B30',
+    BLACK_400: '#303336',
+  },
+  components: {
+    Container: containerTheme,
+    Button: buttonTheme
+  },
+});
+
+export default theme
