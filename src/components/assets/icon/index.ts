bca4cd07d060ffba982fab13903e369ab5be2c2f
anastasia.peretyatko <aperetyatko989@gmail.com>
2025-03-16 13:28:00 +0300

    добавить иконки

---
+++ src/components/assets/icon/index.ts
@@ -0,0 +1,11 @@
+export * from './DashboardIcon';
+
+export * from './KanbanIcon';
+
+export * from './TableIcon';
+
+export * from './ListIcon';
+
+export * from './PaperClipIcon';
+
+export * from './MessageIcon';
